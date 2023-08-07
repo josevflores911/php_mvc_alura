9b9e6d4e66416e5a0f7f52c6fca572935681ac48
J <59713685+josevflores911@users.noreply.github.com>
2023-08-07 17:13:03 -0300

    final del curso

---
+++ config/routes.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'GET|/'=> \Alura\Mvc\Controller\VideoListController::class,
+    'GET|/nuevo-video' => \Alura\Mvc\Controller\VideoFormController::class,
+    'POST|/nuevo-video' => \Alura\Mvc\Controller\NewVideoController::class,
+    'GET|/editar-video'=> \Alura\Mvc\Controller\VideoFormController::class,
+    'POST|/editar-video'=> \Alura\Mvc\Controller\EditVideoController::class,
+];
